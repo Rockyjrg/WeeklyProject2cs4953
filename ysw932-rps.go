f724f584b2b9c9ebbdf30b8f1dfefd8c30ce7dfc
Jamie <jamie.g.ii@hotmail.com>
2025-02-04 19:21:36 -0600

    Added a twist to the rps game by making it into a kind of dungeon crawler game with three different bosses. Used Ai to help refactor my previous code. Runs an infinite loop until the user types exit.

--- ysw932-rps.go
+++ ysw932-rps.go
@@ -3,58 +3,105 @@ package main
 import (
 	"fmt"
 	"math/rand/v2"
+	"strings"
 )
 
-func main() {
-	fmt.Println("We will be playing rock, paper, scissors!")
+type Boss struct {
+	name string
+	move func(string) string
+}
 
-	for {
-		rps := [3]string{"rock", "paper", "scissors"}
-		randomChoice := rand.IntN(len(rps))
-		randomElement := rps[randomChoice]
+// function for game bosses
+func stubbornBoss(userChoice string) string {
+	return "rock"
+}
+
+func ghostBoss(userChoice string) string {
+	choices := []string{"rock", "paper"}
+	return choices[rand.IntN(len(choices))]
+}
+
+func demonBoss(userChoice string) string {
+	//1 in 5 chance
+	if rand.IntN(5) == 0 {
+		return userChoice
+	}
+	choices := []string{"rock", "paper", "scissors"}
+	return choices[rand.IntN(len(choices))]
+}
+
+// function to get random bosses
+func getRandomBoss() Boss {
+	bosses := []Boss{
+		{"Stubborn Boss", stubbornBoss},
+		{"Ghost Boss", ghostBoss},
+		{"Demon Boss", demonBoss},
+	}
+	return bosses[rand.IntN(len(bosses))]
+}
+
+func fightBoss(boss Boss, playerHealth *int) bool {
+	var bossHealth int = 3
+	fmt.Printf("You are fighting %s!\n", boss.name)
+
+	for *playerHealth > 0 && bossHealth > 0 {
 		var userChoice string
-		fmt.Print("Ur move: ")
-		fmt.Scan(&userChoice)
-		fmt.Println("Ai chose: " + randomElement)
-		if userChoice == "rock" {
-			if randomElement == "scissors" {
-				fmt.Println("You win!")
-				continue
-			} else if randomElement == "paper" {
-				fmt.Println("You lose!")
-				continue
+		for {
+			fmt.Println("It is your move. Choose rock, paper, or scissors.")
+			fmt.Scan(&userChoice)
+			userChoice = strings.ToLower(userChoice)
+
+			if userChoice == "exit" {
+				fmt.Println("You ran away, you live to fight again...")
+				return true
+			} else if userChoice == "rock" || userChoice == "paper" || userChoice == "scissors" {
+				break //valid choice
 			} else {
-				fmt.Println("You tie!")
-				continue
+				fmt.Println("Invalid choice. Please enter rock, paper, or scissors.")
 			}
-		} else if userChoice == "paper" {
-			if randomElement == "rock" {
-				fmt.Println("You win!")
-				continue
-			} else if randomElement == "scissors" {
-				fmt.Println("You lose!")
-				continue
-			} else {
-				fmt.Println("You tie!")
-				continue
-			}
-		} else if userChoice == "scissors" {
-			if randomElement == "paper" {
-				fmt.Println("You win!")
-				continue
-			} else if randomElement == "rock" {
-				fmt.Println("You lose!")
-				continue
-			} else {
-				fmt.Println("You tie!")
-				continue
-			}
-		} else if userChoice == "exit" {
-			fmt.Println("Ending game, bye bye!")
-			break
+		}
+
+		var aiChoice string = boss.move(userChoice)
+		fmt.Printf("%s chose: %s\n", boss.name, aiChoice)
+
+		if userChoice == aiChoice {
+			fmt.Println("It's a tie!")
+		} else if (userChoice == "rock" && aiChoice == "scissors") ||
+			(userChoice == "paper" && aiChoice == "rock") ||
+			(userChoice == "scissors" && aiChoice == "paper") {
+			fmt.Println("You landed a hit on the boss!")
+			bossHealth--
 		} else {
-			fmt.Println("Please enter a valid answer.")
-			continue
+			fmt.Println("You got hit!")
+			*playerHealth--
+		}
+		fmt.Printf("Your health: %d | %s health: %d\n", *playerHealth, boss.name, bossHealth)
+	}
+
+	if *playerHealth > 0 {
+		fmt.Println("You have defeated the boss!")
+	} else {
+		fmt.Println("You have been defeated...")
+	}
+
+	return false //player didn't type exit
+}
+
+func main() {
+	fmt.Println("Welcome to RPS dungeon crawl. Type exit to run away")
+
+	var playerHealth int = 5
+
+	for playerHealth > 0 {
+		boss := getRandomBoss()
+		//if the player fightboss returns true exit the game
+		if fightBoss(boss, &playerHealth) {
+			break
+		}
+
+		if playerHealth > 0 {
+			fmt.Println("Moving to the next room...")
 		}
 	}
+	fmt.Print("Game over. Thanks for playing.")
 }
